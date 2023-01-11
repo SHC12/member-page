5b628f685c8d07652138eb5d4442d9371830b22e
shc12 <superhumancrewid12@gmail.com>
2023-01-12 00:02:58 +0700

    chore: init repository

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'assets/css/style.css';
+function App() {
+  return (
+    <div className="App">
+     <div className="container mx-auto" >
+      <h1 className="text-6x1">
+        Javascript React
+      </h1>
+       </div>
+    </div>
+  );
+}
+
+export default App;
